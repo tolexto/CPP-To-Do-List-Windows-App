7068ec89a5593ea8ef756304e8d4763a8a2724a0
Tuna MEYDAN <tolexto@hotmail.com>
2024-01-08 17:39:05 +0300

    First commit

---
+++ ToDoList.cpp
@@ -0,0 +1,182 @@
+﻿// ToDoList.cpp : Uygulamanın giriş noktasını tanımlar.
+//
+
+#include "framework.h"
+#include "ToDoList.h"
+
+#define MAX_LOADSTRING 100
+
+// Genel Değişkenler:
+HINSTANCE hInst;                                // geçerli örnek
+WCHAR szTitle[MAX_LOADSTRING];                  // Başlık çubuğu metni
+WCHAR szWindowClass[MAX_LOADSTRING];            // ana pencere sınıfı adı
+
+// Bu kod modülündeki işlevlerin bildirimlerini ilet:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Kodu buraya yerleştirin.
+
+    // Genel dizeleri başlat
+    wcscpy_s(szTitle, MAX_LOADSTRING, L"CloudList - An app for To-Do List");
+    //LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    //LoadStringW(hInstance, IDC_TODOLIST, szWindowClass, MAX_LOADSTRING);
+    wcscpy_s(szWindowClass, MAX_LOADSTRING, L"MainScreen");
+    MyRegisterClass(hInstance);
+
+    // Uygulama başlatması gerçekleştir:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TODOLIST));
+
+    MSG msg;
+
+    // Ana ileti döngüsü:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  İŞLEV: MyRegisterClass()
+//
+//  AMAÇ: Pencere sınıfını kaydeder.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TODOLIST));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TODOLIST);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   İŞLEV: InitInstance(HINSTANCE, int)
+//
+//   AMAÇ: Örnek tanıtıcısını kaydeder ve ana pencereyi oluşturur
+//
+//   AÇIKLAMALAR:
+//
+//        Bu işlevde, örnek tanıtıcısını genel değişkene kaydederiz ve
+//        ana program penceresini oluşturur ve görüntüleriz.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Örnek tanıtıcısını genel değişkenimizde depola
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  İŞLEV: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  AMAÇ: Ana pencere için iletileri işler.
+//
+//  WM_COMMAND  - uygulama menüsünü işle
+//  WM_PAINT    - Ana pencereyi boya
+//  WM_DESTROY  - bir çıkış iletisi gönder ve dön
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Menü seçimlerini ayrıştır:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Buraya hdc kullanan herhangi bir çizim kodu ekleyin...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Hakkında kutusu için ileti işleyicisi.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
